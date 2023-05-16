2bf6267d44a4783d4d51b14192200d6297e95933
CoolPeachGaming <90976594+CoolPeachGaming@users.noreply.github.com>
2023-05-15 23:31:12 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#Aden Choudhury
+ #Aden Choudhury
 #Reference PyGame as pg to save typing time
 import pygame as pg
 import sys
@@ -9,6 +9,9 @@ from object_renderer import *
 from raycasting import *
 from sprite_object import *
 from object_handler import *
+from weapon import *
+from sound import *
+from pathfinding import *
 
 class Game:
   def __init__(self):
@@ -17,6 +20,9 @@ class Game:
     self.screen = pg.display.set_mode(RES)
     self.clock = pg.time.Clock()
     self.delta_time = 1
+    self.global_trigger = False
+    self.global_event = pg.USEREVENT + 0
+    pg.time.set_timer(self.global_event, 40)
     self.new_game()
 
   def new_game(self):
@@ -25,11 +31,15 @@ class Game:
     self.object_renderer = ObjectRenderer(self)
     self.raycasting = RayCasting(self)
     self.object_handler = ObjectHandler(self)
+    self.sound = Sound(self)
+    self.weapon = Weapon(self)
+    self.pathfinding = PathFinding(self)
 
   def update(self):
     self.player.update()
     self.raycasting.update()
     self.object_handler.update()
+    self.weapon.update()
   #update contents of display and display FPS as the window caption
     pg.display.flip()
     self.delta_time = self.clock.tick(FPS)
@@ -38,14 +48,19 @@ class Game:
   def draw(self):
     #self.screen.fill("black")
     self.object_renderer.draw()
+    self.weapon.draw()
     #self.map.draw()
     #self.player.draw()
 
   def check_events(self):
+    self.global_trigger = False
     for event in pg.event.get():
       if event.type == pg.QUIT or (event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE):
         pg.quit
         sys.exit()
+      elif event.type == self.global_event:
+        self.global_trigger = True
+      self.player.single_fire_event(event)
 
   def run(self):
     while True:
